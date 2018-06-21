d641fab1f77a2bce63c35856e901b49344bb2f2c
darkobits <joshua@frontlawn.net>
2018-06-20 22:09:24 -0700

    refactor: Use TypeScript.

--- README.md
+++ README.md
@@ -1,6 +1,13 @@
-![log](https://user-images.githubusercontent.com/441546/36626870-6736615a-18ef-11e8-8e5e-1c737f0fbb40.png)
-
-[![][npm-img]][npm-url] [![][travis-img]][travis-url] [![][david-img]][david-url] [![][david-dev-img]][david-dev-url] [![][cc-img]][cc-url] [![][xo-img]][xo-url]
+<a href="#top" id="top">
+  <img src="https://user-images.githubusercontent.com/441546/41699021-76157614-74d6-11e8-9ad0-13708b41176e.png" style="max-width: 100%">
+</a>
+<p align="center">
+  <a href="https://www.npmjs.com/package/@darkobits/log"><img src="https://img.shields.io/npm/v/@darkobits/log.svg?style=flat-square"></a>
+  <a href="https://travis-ci.org/darkobits/log"><img src="https://img.shields.io/travis/darkobits/log.svg?style=flat-square"></a>
+  <a href="https://david-dm.org/darkobits/log"><img src="https://img.shields.io/david/darkobits/log.svg?style=flat-square"></a>
+  <a href="https://github.com/conventional-changelog/standard-version"><img src="https://img.shields.io/badge/conventional%20commits-1.0.0-027dc6.svg?style=flat-square"></a>
+  <a href="https://github.com/sindresorhus/xo"><img src="https://img.shields.io/badge/code_style-XO-e271a5.svg?style=flat-square"></a>
+</p>
 
 A wrapper around the wonderful [`npmlog`](https://github.com/npm/npmlog) that allows for the creation of unique instances.
 
---
+++ src/log.ts
@@ -0,0 +1,46 @@
+// @ts-ignore
+import importUnique from '@darkobits/import-unique';
+
+
+/**
+ * Imports a unique copy of "npmlog", optionally setting the heading and level.
+ */
+export default function NpmLogFactory(heading?: string, level?: string) {
+  // Unique instance of npmlog.
+  const log = importUnique('npmlog');
+
+  const {LOG_LEVEL} = process.env;
+
+  // Predicate which returns true if npmlog has the provided log level.
+  const hasLevel = (l: string) => Object.keys(log.levels).includes(l);
+
+  // Optionally set the heading.
+  if (heading) {
+    log.heading = heading;
+  }
+
+  // Optionally validate and set the log level.
+  if (level) {
+    if (hasLevel(level)) {
+      log.level = level;
+    } else {
+      throw new Error(`Unsupported log level: "${level}".`);
+    }
+  } else if (LOG_LEVEL && hasLevel(LOG_LEVEL)) {
+    log.level = LOG_LEVEL;
+  }
+
+  // Replace each log method with one that, when provided an instance of Error,
+  // simply logs its stack.
+  Object.keys(log.levels).forEach((level: string) => {
+    const origMethod = log[level];
+
+    log[level] = (prefix: string, ...args: Array<any>) => {
+      return Reflect.apply(origMethod, log, [prefix, ...args.map(arg => {
+        return arg instanceof Error ? arg.stack : arg;
+      })]);
+    };
+  });
+
+  return log;
+}
